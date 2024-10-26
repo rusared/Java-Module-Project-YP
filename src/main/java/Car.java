2b38ed1c1a964baf729559af775754ce67ccba94
arrus8 <rusared@ya.ru>
2024-10-26 05:04:17 +0500

    Создал класс Автомобиля

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Car {
+    String name;
+    int speed;
+
+    public Car (String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+}
