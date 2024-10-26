155a001dad4f08e4556b738b7da7484a469ce16a
arrus8 <rusared@ya.ru>
2024-10-26 05:24:39 +0500

    Сделал класс Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+package org.example;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        ArrayList<Car> cars = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i = i + 1) {
+            System.out.println(String.format("Введите название машины №%d:", i));
+            String name = scanner.next();
+            System.out.println(String.format("Введите скорость машины №%d:", i));
+            while (true) {
+                int speed = scanner.nextInt();
+                Car car = new Car(name, speed);
+                if (speed >= 0 && speed <= 250) {
+                    cars.add(car);
+                    race.raceLeader(car);
+                    break;
+                }
+                System.out.println("Некорректная скорость!\nДопустимая скорость для автомобилей от 0 до 250 км/ч.\nПожалуйста, повторите ввод.");
+            }
+
+        }
+
+        System.out.println(String.format("Самая быстрая машина: %s", race.getLeader()));
     }
 }
